aa0c4f626df3c0b452468bb079bcd5649959c23e
David Dropping <ddropping@gmail.com>
2020-03-23 14:50:52 -0700

    Update resume page add transition status

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { TransitionState } from "gatsby-plugin-transition-link"
 import Img from "gatsby-image"
+import { motion } from "framer-motion"
 
 const Resume = () => {
   const data = useStaticQuery(graphql`
@@ -14,13 +16,50 @@ const Resume = () => {
       }
     }
   `)
+
+  const variants = {
+    contentHide: {
+      y: 100,
+      opacity: 0,
+      transition: {
+        type: "spring",
+        damping: 200,
+        mass: 0.1,
+      },
+    },
+    contentShow: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        damping: 20,
+      },
+    },
+  }
+
   return (
-    <div style={{ maxWidth: "1200px" }}>
-      <Img
-        style={{ maxWidth: "700px", margin: "auto" }}
-        fluid={data.resume.childImageSharp.fluid}
-      />
-    </div>
+    <TransitionState>
+      {({ transitionStatus }) => {
+        return (
+          <motion.div
+            variants={variants}
+            initial={"contentHide"}
+            animate={
+              ["entering", "entered"].includes(transitionStatus)
+                ? "contentShow"
+                : "contentHide"
+            }
+          >
+            <div style={{ maxWidth: "1200px" }}>
+              <Img
+                style={{ maxWidth: "700px", margin: "auto" }}
+                fluid={data.resume.childImageSharp.fluid}
+              />
+            </div>
+          </motion.div>
+        )
+      }}
+    </TransitionState>
   )
 }
 
