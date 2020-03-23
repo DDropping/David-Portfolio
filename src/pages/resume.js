0b459e6a4a5488743c7ed6301b59e455ac640ea5
David Dropping <ddropping@gmail.com>
2020-03-23 14:43:55 -0700

    Create resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,11 +1,27 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const resume = () => {
+const Resume = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      resume: file(relativePath: { eq: "resume.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_noBase64
+          }
+        }
+      }
+    }
+  `)
   return (
-    <div>
-      <div>resume</div>
+    <div style={{ maxWidth: "1200px" }}>
+      <Img
+        style={{ maxWidth: "700px", margin: "auto" }}
+        fluid={data.resume.childImageSharp.fluid}
+      />
     </div>
   )
 }
 
-export default resume
+export default Resume
